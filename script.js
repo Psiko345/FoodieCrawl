00eee121eed180e2ad6f47d572f1788181d6bfc6
Andrew Bui <andrew.bui.au@gmail.com>
2020-06-25 21:37:01 +1000

    added api and ajax code to script.js

---
+++ script.js
@@ -0,0 +1,35 @@
+// Zomato
+$("#zomato").on("click", function () {
+    // Sydney id: 260
+    const zomatoQueryURL = "https://developers.zomato.com/api/v2.1/cuisines?city_id=260";
+
+    $.ajax({
+        url: zomatoQueryURL,
+        method: "GET",
+        headers: { "user-key": "1a1c38e54e42f2ec4d53164ce7ba37ed" }
+    })
+
+        .then(function (response) {
+            console.log(response);
+        });
+});
+
+
+// Nutritionix
+$("#nutritionix").on("click", function () {
+
+    const nutritionixURL = "https://nutritionix-api.p.rapidapi.com/v1_1/search/mcdonalds?fields=nf_calories%252Cnf_total_fat";
+
+    $.ajax({
+        url: nutritionixURL,
+        method: "GET",
+        headers: {
+            "x-rapidapi-host": "nutritionix-api.p.rapidapi.com",
+            "x-rapidapi-key": "1a2458beb0mshc0c5065ac438661p1dc054jsnb4bee91478de"
+        }
+    })
+
+        .then(function (response) {
+            console.log(response);
+        });
+});
