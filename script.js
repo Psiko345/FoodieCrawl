427cf1cf392766a96f0ab07e5367d8b6f68738b9
Joshua Piper <joshua.g.piper@gmail.com>
2020-06-28 21:31:48 +1000

    got ajax calls working

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
+window.onload = function () {
+  getNutrition();
+  getZomato();
+};
+
 // Zomato
-$("#zomato").on("click", function () {
+function getZomato() {
   // Sydney id: 260
   const zomatoQueryURL =
     "https://developers.zomato.com/api/v2.1/cuisines?city_id=260";
@@ -8,14 +13,13 @@ $("#zomato").on("click", function () {
     url: zomatoQueryURL,
     method: "GET",
     headers: { "user-key": "1a1c38e54e42f2ec4d53164ce7ba37ed" },
-  })
-  .then(function (response) {
+  }).then(function (response) {
     console.log(response);
   });
-});
+}
 
 // Nutritionix
-$("#nutritionix").on("click", function () {
+function getNutrition() {
   const nutritionixURL =
     "https://nutritionix-api.p.rapidapi.com/v1_1/search/mcdonalds?fields=nf_calories%252Cnf_total_fat";
 
@@ -26,8 +30,7 @@ $("#nutritionix").on("click", function () {
       "x-rapidapi-host": "nutritionix-api.p.rapidapi.com",
       "x-rapidapi-key": "1a2458beb0mshc0c5065ac438661p1dc054jsnb4bee91478de",
     },
-  })
-  .then(function (response) {
+  }).then(function (response) {
     console.log(response);
   });
-});
+}
